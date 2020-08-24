6544ff52689cacd82a3a420dfe7d63a95f9687ff
HazelicEyes <henriquepromail9863@gmail.com>
2020-08-23 22:37:43 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("qualquer coisa pra printar")
+a = 547
+b = 645
+soma = a + b
+subi = a - b
+multi = a * b
+divi = a / b
+print("A Soma é:", soma)
+print("A Subtração é:", subi)
+print("A Multiplicação é:", multi)
+print("A Divisão é:", divi)
