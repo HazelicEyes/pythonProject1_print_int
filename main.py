f794efdea364b5a1abb6480100daee7b5887373d
HazelicEyes <henriquepromail9863@gmail.com>
2020-08-25 22:23:19 -0300

    Edit no primeiro print Separação de prints incluir soma2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("qualquer coisa pra printar")
+print("Teste de resultados")
 a = 547
 b = 645
 soma = a + b
@@ -6,8 +6,14 @@ subi = a - b
 multi = a * b
 divi = a / b
 resto = a % b
+print("A é:", a)
+print("B é:", b)
+print("-------------------------------")
 print("A Soma é:", soma)
 print("A Subtração é:", subi)
 print("A Multiplicação é:", multi)
 print("A Divisão é:", divi)
 print("O Resto da divisão é:", resto)
+print("-------------------------------")
+soma2 = a + 99
+print("A + 99 é:", soma2)
