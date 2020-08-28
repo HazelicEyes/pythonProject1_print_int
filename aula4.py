d02f488fd9101a61b4d432b4729da4d693e6bbdb
HazelicEyes <henriquepromail9863@gmail.com>
2020-08-28 01:04:03 -0300

    aula4 for while

---
+++ aula4.py
@@ -0,0 +1,62 @@
+
+a = 0
+while a <= 10:
+    print(a)
+    a += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = int(input("Quero saber todos os numeros primos ate: "))
+# for num in range(a+1):
+#     div = 0
+#     for x in range(1, num+1):
+#         resto = num % x
+#         # print(x, resto)
+#         if resto == 0:
+#             div += 1
+#     if div == 2:
+#         print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = int(input("Entre com o numero: "))
+#
+# div = 0
+# for x in range(1, a+1):
+#     resto = a % x
+#     print(x, resto)
+#     if resto == 0:
+#         div += 1
+#
+# if div == 2:
+#     print("Numero {} é primo".format(a))
+# else:
+#     print("Numero não é primo")
